9d4861ef8cf9812e70ed8a7dd7ed04cd6aeaa9d7
Noah Patton <noah.patton1225@gmail.com>
2026-09-18 17:35:50 -0700

    added metrics and reset

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) incrementHits(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		handler.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/healthz", healthzHandler)
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("filepathroot"))))
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+
+	mux.HandleFunc("/reset", cfg.resetHandler)
+
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.incrementHits(http.FileServer(http.Dir(filepathRoot)))))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -28,4 +48,17 @@ func healthzHandler(writer http.ResponseWriter, req *http.Request) {
 	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	cfg.fileserverHits.Swap(0)
+	writer.Write([]byte("Metrics Reset"))
 }
